ef836ba502607879c36b49894913022fdf3fe82f
Anton <82974569+AntooxaD@users.noreply.github.com>
2021-09-18 21:24:49 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -15,42 +15,22 @@
 //   после чего вставит все li за одну операцию в список ul.ingredients.Для создания DOM - узлов
 // используй document.createElement().
 
-// const ingredients = [
-//   'Картошка',
-//   'Грибы',
-//   'Чеснок',
-//   'Помидоры',
-//   'Зелень',
-//   'Приправы',
-// ]
-// const ul = document.getElementById("ingredients");
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+]
+const ul = document.getElementById("ingredients");
 
-// const ingredient = ingredients.reduce((element, index) => element + "\n" + `<li>${index}</li>`, "")
+const ingredient = ingredients.reduce((element, index) => element + `<li>${index}</li>`, "")
 
-// ul.innerHTML = ingredient;
+ul.innerHTML = ingredient;
 
-// console.log(ingredient)
+console.log(ingredient)
 
 
   
-
-
-const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы"
-];
-const foods = document.querySelector("#ingredients");
-const food = ingredients.forEach(ingredient => {
-  const li = document.createElement("li");
-    const text = document.createElement('text');
-    li.appendChild(text);
-    foods.appendChild(li)
-    text.textContent = ingredient;
-    
-    console.log(ingredient);
-});
   
